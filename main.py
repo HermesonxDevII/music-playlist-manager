97e65d1aa58b80e9e9a44c5e720c2f0764a965e5
HermesonxDev <hermesoninfo@gmail.com>
2025-09-13 11:24:09 -0300

    feat: adding view_playlist and get_longest_song functions

--- main.py
+++ main.py
@@ -20,3 +20,18 @@ def add_song(title: str, artist: str, genre: str, duration: int) -> None:
     }
     playlist.append(song)
     print(f"\n✅ A música '{title}' foi adicionada a playlist com sucesso!")
+
+def view_playlist():
+    if not playlist:
+        print("\n A playlist esta vazia.")
+        return
+    
+    print("\n--- Sua Playlist ---")
+    for i, song in enumerate(playlist, start=1):
+        print(f"{i}. '{song['title']}' de {song['artist']} ({song['genre']}) - {song['duration']}s")
+    print("---------------------")
+
+def get_longest_song() -> Optional[Song]:
+    if not playlist:
+        return None
+    return max(playlist, key=lambda song: song['duration'])
